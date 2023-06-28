2db3cbd29f3ca2568212108fcde19fa3a766aeb9
Lefterisk0025 <43676375+Lefterisk0025@users.noreply.github.com>
2023-06-28 20:50:04 +0300

    Create Contact page

--- career-app-client/src/pages/contact.js
+++ career-app-client/src/pages/contact.js
@@ -1,9 +1,18 @@
+import HomeLayout from "@/components/home.layout";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { useSession } from "next-auth/react";
+
 export default function Contact() {
   return (
     <>
       <h1 className="mt-3" style={{ textAlign: "center" }}>
-        Contact
+        CONTACT
       </h1>
     </>
   );
 }
+
+Contact.getLayout = function getLayout(page) {
+  return <HomeLayout>{page}</HomeLayout>;
+};
